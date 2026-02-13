20c397664cfeb82b7bc4b716243810839577206d
AgoAnd <14530111@itisrossi.vi.it>
2026-02-13 22:33:51 +0100

    Refactor ticket management and add new features
    Refactor ticket management system with improved structure and functionality.

--- cinema/cinema/Program.cs
+++ cinema/cinema/Program.cs
@@ -1,173 +1,246 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
-enum TipoBiglietto
+namespace esBiglietto
 {
-    standard,
-    ridotto,
-    vip
-}
-
-enum StatoPrenotazione
-{
-    prenotato,
-    pagato,
-    annullato
-}
-
-struct Biglietto
-{
-    public int numeroDiPosto;
-    public TipoBiglietto tipo;
-    public StatoPrenotazione statoPrenotazione;
-    public DateTime ddata;
-}
-
-class Program
-{
-    static List<Biglietto> biglietti = new List<Biglietto>();
-
-    static void Main()
+    enum TipoBiglietto
     {
-        char r;
-        int p = 2;
-        do
+        Intero,
+        Ridotto,
+        VIP
+    }
+
+    enum StatoPrenotazione
+    {
+        Prenotato,
+        Pagato,
+        Annullato
+    }
+
+    struct Biglietto
+    {
+        public int id;
+        public int numeroDiPosto;
+        public TipoBiglietto tipo;
+        public StatoPrenotazione stato;
+        public DateTime data;
+
+        public Biglietto(int id, int numeroDiPosto, TipoBiglietto tipo, DateTime data)
         {
-            int s;
+            this.id = id;
+            this.numeroDiPosto = numeroDiPosto;
+            this.tipo = tipo;
+            this.stato = StatoPrenotazione.Prenotato;
+            this.data = data;
+        }
 
-            Console.WriteLine("Inserisci il numero dell'operazione:");
-            Console.WriteLine("1) crea un biglietto");
-            Console.WriteLine("2) pagare un biglietto");
-            Console.WriteLine("3) annullare un biglietto");
-            Console.WriteLine("4) visualizzare biglietti");
+        public string GetInfo()
+        {
+            return "Biglietto " + id +
+                   ": Posto = " + numeroDiPosto +
+                   ", Tipologia = " + tipo +
+                   ", Stato = " + stato +
+                   ", Data = " + data;
+        }
 
-            bool v = int.TryParse(Console.ReadLine(), out s);
-            while (!v || s < 1 || s > 4)
+        public void CambiaStato(StatoPrenotazione nuovoStato)
+        {
+            this.stato = nuovoStato;
+        }
+    }
+
+    class Program
+    {
+        static List<Biglietto> memory = new List<Biglietto>();
+        static int numberOfTickets = 1;
+
+        static void Main(string[] args)
+        {
+            int action;
+            bool loop = true;
+
+            while (loop)
             {
-                Console.WriteLine("errore");
-                v = int.TryParse(Console.ReadLine(), out s);
-            }
+                Console.WriteLine();
+                Console.WriteLine("1. Crea un biglietto");
+                Console.WriteLine("2. Elimina un biglietto");
+                Console.WriteLine("3. Modifica un biglietto");
+                Console.WriteLine("4. Visualizza i biglietti");
+                Console.WriteLine("5. Esci");
+                Console.WriteLine();
+                Console.WriteLine("Inserisci azione:");
 
-            // 1) crea biglietto
-            if (s == 1)
-            {
-                Biglietto b = new Biglietto();
+                bool control = int.TryParse(Console.ReadLine(), out action);
 
-                Console.Write("Inserisci numero di posto: ");
-                bool v1 = int.TryParse(Console.ReadLine(), out b.numeroDiPosto);
-                while (!v1 || b.numeroDiPosto <= 0)
+                while (!control || action < 1 || action > 5)
                 {
-                    Console.WriteLine("errore");
-                    v1 = int.TryParse(Console.ReadLine(), out b.numeroDiPosto);
+                    Console.WriteLine("Numero non valido, reinserire (1-5):");
+                    control = int.TryParse(Console.ReadLine(), out action);
                 }
 
-                Console.WriteLine("Tipo biglietto:");
-                Console.WriteLine("0) standard");
-                Console.WriteLine("1) ridotto");
-                Console.WriteLine("2) vip");
-
-                int t;
-                bool v2 = int.TryParse(Console.ReadLine(), out t);
-                while (!v2 || t < 0 || t > 2)
+                switch (action)
                 {
-                    Console.WriteLine("errore");
-                    v2 = int.TryParse(Console.ReadLine(), out t);
-                }
+                    case 1:
+                        Biglietto ticket = CreaBiglietto(numberOfTickets);
+                        memory.Add(ticket);
+                        numberOfTickets++;
+                        break;
 
-                b.tipo = (TipoBiglietto)t;
-                b.statoPrenotazione = StatoPrenotazione.prenotato;
-                b.ddata = DateTime.Now;
+                    case 2:
+                        EliminaBiglietto();
+                        break;
 
-                biglietti.Add(b);
-                Console.WriteLine("Biglietto creato");
-            }
+                    case 3:
+                        ModificaBiglietto();
+                        break;
 
-            // 2) pagare biglietto
-            if (s == 2)
-            {
-                if (biglietti.Count == 0)
-                {
-                    Console.WriteLine("Nessun biglietto disponibile");
-                }
-                else
-                {
-                    for (int i = 0; i < biglietti.Count; i++)
-                    {
-                        Biglietto temp = biglietti[i];
-                        Console.WriteLine(i + ") Posto: " + temp.numeroDiPosto + " Stato: " + temp.statoPrenotazione);
-                    }
+                    case 4:
+                        VisualizzaBiglietti();
+                        break;
 
-                    Console.Write("Scegli indice biglietto: ");
-                    int iScelta;
-                    bool v3 = int.TryParse(Console.ReadLine(), out iScelta);
-                    while (!v3 || iScelta < 0 || iScelta >= biglietti.Count)
-                    {
-                        Console.WriteLine("errore");
-                        v3 = int.TryParse(Console.ReadLine(), out iScelta);
-                    }
-
-                    Biglietto b = biglietti[iScelta];
-                    b.statoPrenotazione = StatoPrenotazione.pagato;
-                    biglietti[iScelta] = b;
-
-                    Console.WriteLine("Biglietto pagato");
+                    case 5:
+                        loop = false;
+                        break;
                 }
             }
 
-            // 3) annullare biglietto
-            if (s == 3)
+            Console.WriteLine("Arrivederci!");
+        }
+
+        static Biglietto CreaBiglietto(int number)
+        {
+            int posto;
+            int tipologia;
+            bool control;
+
+            Console.WriteLine("Inserire numero posto:");
+            control = int.TryParse(Console.ReadLine(), out posto);
+
+            while (!control || posto <= 0)
             {
-                if (biglietti.Count == 0)
+                Console.WriteLine("Numero non valido, reinserire:");
+                control = int.TryParse(Console.ReadLine(), out posto);
+            }
+
+            Console.WriteLine("Tipologia (0=Intero, 1=Ridotto, 2=VIP):");
+            control = int.TryParse(Console.ReadLine(), out tipologia);
+
+            while (!control || tipologia < 0 || tipologia > 2)
+            {
+                Console.WriteLine("Tipologia non valida, reinserire (0-2):");
+                control = int.TryParse(Console.ReadLine(), out tipologia);
+            }
+
+            Console.WriteLine("Biglietto creato!");
+
+            return new Biglietto(number, posto, (TipoBiglietto)tipologia, DateTime.Now);
+        }
+
+        static void EliminaBiglietto()
+        {
+            if (memory.Count == 0)
+            {
+                Console.WriteLine("Nessun biglietto da eliminare.");
+                return;
+            }
+
+            int id;
+            bool control;
+
+            Console.WriteLine("Inserire ID del biglietto da eliminare:");
+            control = int.TryParse(Console.ReadLine(), out id);
+
+            while (!control)
+            {
+                Console.WriteLine("Numero non valido, reinserire:");
+                control = int.TryParse(Console.ReadLine(), out id);
+            }
+
+            bool trovato = false;
+
+            for (int i = 0; i < memory.Count; i++)
+            {
+                if (memory[i].id == id)
                 {
-                    Console.WriteLine("Nessun biglietto disponibile");
-                }
-                else
-                {
-                    for (int i = 0; i < biglietti.Count; i++)
-                    {
-                        Biglietto temp = biglietti[i];
-                        Console.WriteLine(i + ") Posto: " + temp.numeroDiPosto + " Stato: " + temp.statoPrenotazione);
-                    }
-
-                    Console.Write("Scegli indice biglietto: ");
-                    int iScelta;
-                    bool v4 = int.TryParse(Console.ReadLine(), out iScelta);
-                    while (!v4 || iScelta < 0 || iScelta >= biglietti.Count)
-                    {
-                        Console.WriteLine("errore");
-                        v4 = int.TryParse(Console.ReadLine(), out iScelta);
-                    }
-
-                    Biglietto b = biglietti[iScelta];
-                    b.statoPrenotazione = StatoPrenotazione.annullato;
-                    biglietti[iScelta] = b;
-
-                    Console.WriteLine("Biglietto annullato");
+                    memory.RemoveAt(i);
+                    trovato = true;
+                    Console.WriteLine("Biglietto eliminato.");
+                    break;
                 }
             }
 
-            // 4) visualizzare biglietti
-            if (s == 4)
+            if (!trovato)
             {
-                if (biglietti.Count == 0)
+                Console.WriteLine("Biglietto non trovato.");
+            }
+        }
+
+        static void ModificaBiglietto()
+        {
+            if (memory.Count == 0)
+            {
+                Console.WriteLine("Nessun biglietto da modificare.");
+                return;
+            }
+
+            int id;
+            int stato;
+            bool control;
+
+            Console.WriteLine("Inserire ID del biglietto da modificare:");
+            control = int.TryParse(Console.ReadLine(), out id);
+
+            while (!control)
+            {
+                Console.WriteLine("Numero non valido, reinserire:");
+                control = int.TryParse(Console.ReadLine(), out id);
+            }
+
+            bool trovato = false;
+
+            for (int i = 0; i < memory.Count; i++)
+            {
+                if (memory[i].id == id)
                 {
-                    Console.WriteLine("Nessun biglietto creato");
-                }
-                else
-                {
-                    for (int i = 0; i < biglietti.Count; i++)
+                    Console.WriteLine("Nuovo stato (0=Prenotato,1=Pagato,2=Annullato):");
+                    control = int.TryParse(Console.ReadLine(), out stato);
+
+                    while (!control || stato < 0 || stato > 2)
                     {
-                        Biglietto temp = biglietti[i];
-                        Console.WriteLine(i + ") Posto: " + temp.numeroDiPosto +
-                                          " Tipo: " + temp.tipo +
-                                          " Stato: " + temp.statoPrenotazione +
-                                          " Data: " + temp.ddata.ToShortDateString());
+                        Console.WriteLine("Stato non valido, reinserire (0-2):");
+                        control = int.TryParse(Console.ReadLine(), out stato);
                     }
+
+                    Biglietto temp = memory[i];
+                    temp.CambiaStato((StatoPrenotazione)stato);
+                    memory[i] = temp;
+
+                    Console.WriteLine("Stato modificato.");
+                    trovato = true;
+                    break;
                 }
             }
 
+            if (!trovato)
+            {
+                Console.WriteLine("Biglietto non trovato.");
+            }
+        }
 
-         
-        } while (p != 0);
+        static void VisualizzaBiglietti()
+        {
+            if (memory.Count == 0)
+            {
+                Console.WriteLine("Nessun biglietto presente.");
+            }
+            else
+            {
+                for (int i = 0; i < memory.Count; i++)
+                {
+                    Console.WriteLine(memory[i].GetInfo());
+                }
+            }
+        }
     }
 }
+
